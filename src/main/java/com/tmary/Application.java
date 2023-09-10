d2a380f6df415dc36a8179a2815b5ed3e511d790
Nadav Dayan <ndayan@appdev.yearup.org>
2023-09-09 20:02:31 -0400

    dateRange() returns date of previous week from last Sunday to last Saturday

--- src/main/java/com/tmary/Application.java
+++ src/main/java/com/tmary/Application.java
@@ -1,40 +1,57 @@
 package com.tmary;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-
 
 public class Application {
 
+    static Date today = new Date();
+    static SimpleDateFormat fmt = new SimpleDateFormat("E, MM/dd/yyyy");
+    static SimpleDateFormat dayFormat = new SimpleDateFormat("E");
+
     public static void main(String[] args) {
         //runs today's date and determines the range of events
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("E, MM/dd/yyyy");
-        String formattedDate = today.format(fmt);
+        String formattedDate = fmt.format(today);
         System.out.println("Today is: " + formattedDate);
 
-        Date lastWeek = new Date();
+        Date[] range = dateRange();
+
+        for (int i = 0; i < range.length; i++){
+            System.out.println(range[i]);
+        }
+
+        /*Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR,-1);
+        Date yesterday = calendar.getTime();
+        String yesterdayFormatted = fmt.format(yesterday);
+        System.out.println("Yesterday was: " + yesterdayFormatted);*/
+
         //searches array list of Members in said range
 
         //Displays the range
     }
 
-    public static int dateRange () {
+    public static Date[] dateRange () {
         // returns the start date and end date in an array
-        Date[] range;
+        Date[] range = new Date[2];
+        Calendar calendar = Calendar.getInstance();
+        String dayOfWeek = dayFormat.format(today);
 
         // reads today's date and determines if it is a Sunday by a numerical value
-        LocalDate today = LocalDate.now();
-        DayOfWeek day = today.getDayOfWeek();
+        if (!dayOfWeek.equalsIgnoreCase("Sun")) {
+            while (!dayOfWeek.equalsIgnoreCase("Sun")) {
+                calendar.add(Calendar.DAY_OF_YEAR, -1);
+                Date referecnceDate = calendar.getTime();
+                dayOfWeek = dayFormat.format(referecnceDate);
 
-        if (day.getValue() == 7) {
+            }
 
         }
-        return day.getValue();
+        calendar.add(Calendar.DAY_OF_YEAR,-1);
+        range[0] = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR,-6);
+        range[1] = calendar.getTime();
+        return range;
     }
 }
